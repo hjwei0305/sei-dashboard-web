e7ad9a38f6cb862eb06277527230a6925fa64da0
eason <cheng.yi@capitalcrux.com>
2020-05-13 10:52:04 +0800

    解决IE11兼容问题

--- src/pages/Screen/model.js
+++ src/pages/Screen/model.js
@@ -1,65 +1,72 @@
-import { message } from "antd";
+import { message } from 'antd';
 import { utils } from 'suid';
-import { getSceneByCode } from "./service";
+import { getSceneByCode } from './service';
 
 const { pathMatchRegexp, dvaModel } = utils;
 const { modelExtend, model } = dvaModel;
 
 export default modelExtend(model, {
-    namespace: "screenView",
+  namespace: 'screenView',
 
-    state: {
-        currentScreenTemplate: '',
-        globalConfig: {},
-        templateConfig: {},
-        instanceDtos: [],
+  state: {
+    currentScreenTemplate: '',
+    globalConfig: {},
+    templateConfig: {},
+    instanceDtos: [],
+  },
+  subscriptions: {
+    setup({ dispatch, history }) {
+      history.listen(location => {
+        const match = pathMatchRegexp('/scene/screenView/:code', location.pathname);
+        if (match) {
+          dispatch({
+            type: 'getScene',
+            payload: {
+              code: match[1],
+              home: false,
+            },
+          });
+        }
+      });
     },
-    subscriptions: {
-        setup({ dispatch, history }) {
-            history.listen(location => {
-                const match = pathMatchRegexp('/scene/screenView/:code', location.pathname);
-                if (match) {
-                    dispatch({
-                        type: "getScene",
-                        payload: {
-                            code: match[1],
-                            home: false,
-                        }
-                    });
-                }
-            });
-        },
+  },
+  effects: {
+    *getScene({ payload }, { call, put }) {
+      const { code } = payload;
+      const re = yield call(getSceneByCode, { code });
+      if (re.success) {
+        let currentScreenTemplate = '';
+        let instanceDtos = [];
+        let templateConfig = {};
+        let globalConfig = {};
+        const { config, instanceDtos: originInstanceDtos } = re.data || {
+          config: null,
+          instanceDtos: [],
+        };
+        if (config) {
+          const configData = JSON.parse(config);
+          const {
+            screenTemplate,
+            templateConfig: originTemplateConfig,
+            globalConfig: originGlobalConfig,
+          } = configData;
+          currentScreenTemplate = screenTemplate;
+          instanceDtos = originInstanceDtos;
+          templateConfig = originTemplateConfig;
+          globalConfig = originGlobalConfig;
+        }
+        yield put({
+          type: 'updateState',
+          payload: {
+            currentScreenTemplate,
+            globalConfig,
+            instanceDtos,
+            templateConfig,
+          },
+        });
+      } else {
+        message.error(re.message);
+      }
     },
-    effects: {
-        * getScene({ payload }, { call, put }) {
-            const { code } = payload;
-            const re = yield call(getSceneByCode, { code });
-            if (re.success) {
-                let currentScreenTemplate = "";
-                let instanceDtos = [];
-                let templateConfig = {};
-                let globalConfig = {};
-                const { config, instanceDtos: originInstanceDtos } = re.data || { instanceDtos: [] };
-                if (config) {
-                    const configData = JSON.parse(config);
-                    const { screenTemplate, templateConfig: originTemplateConfig, globalConfig: originGlobalConfig } = configData;
-                    currentScreenTemplate = screenTemplate;
-                    instanceDtos = originInstanceDtos;
-                    templateConfig = originTemplateConfig;
-                    globalConfig = originGlobalConfig;
-                }
-                yield put({
-                    type: "updateState",
-                    payload: {
-                        currentScreenTemplate,
-                        globalConfig,
-                        instanceDtos,
-                        templateConfig,
-                    }
-                });
-            } else {
-                message.error(re.message);
-            }
-        },
-    }
+  },
 });
