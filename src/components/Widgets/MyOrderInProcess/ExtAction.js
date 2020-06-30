85057755d18d4f350a41bc13189617e04073dd4f
eason <cheng.yi@capitalcrux.com>
2020-06-30 09:06:15 +0800

    UE

--- src/components/Widgets/MyOrderInProcess/ExtAction.js
+++ src/components/Widgets/MyOrderInProcess/ExtAction.js
@@ -123,7 +123,7 @@ class ExtAction extends PureComponent {
       <>
         {menusData.length > 0 ? (
           <Dropdown
-            trigger={['hover', 'click']}
+            trigger={['click']}
             overlay={this.getMenu(menusData, doneItem)}
             className="action-drop-down"
             placement="bottomLeft"
